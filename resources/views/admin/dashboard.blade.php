5a099f622c6ed51ea7ee64f0a2a5e426c1d56627
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-05-30 08:22:44 +0700

    `update` dashboard view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,23 @@
 @extends('admin.layouts.app')
+
+@section('content')
+    <div class="container"></div>
+@endsection
+
+@section('script')
+    <script>
+        //Notify
+        $.notify({
+            icon: 'flaticon-alarm-1',
+            title: 'UMKMPlus Admin',
+            message: 'Selamat Datang di Dashboard UMKMPlus',
+        }, {
+            type: 'secondary',
+            placement: {
+                from: "bottom",
+                align: "right"
+            },
+            time: 2000,
+        });
+    </script>
+@endsection
