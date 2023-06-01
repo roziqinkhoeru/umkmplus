fc15b02813af01ae9ac96645a745a64e4a5c5c35
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-06-01 16:32:17 +0700

    `create` student admin view

---
+++ resources/views/admin/students/index.blade.php
@@ -0,0 +1,92 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="page-inner">
+            {{-- header --}}
+            <div class="page-header">
+                <h4 class="page-title">Student</h4>
+                <ul class="breadcrumbs">
+                    <li class="nav-home">
+                        <a href="/admin">
+                            <i class="flaticon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">
+                            Data Pelajar
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            {{-- main content --}}
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-head-row">
+                                <div class="card-title">Data Pelajar</div>
+                                <div class="card-tools">
+                                    <a href="#" class="btn btn-info btn-border btn-round btn-sm mr-2">
+                                        <span class="btn-label">
+                                        </span>
+                                        Tambah Pelajar
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="studentTable" class="display table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Kelas</th>
+                                            <th>Status</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Jhon Doe</td>
+                                            <td>doejhon@mail.com</td>
+                                            <td>
+                                                <ol class="mb-0 pl-3">
+                                                    <li class="mb-1">TitleCourse (categoryCourse)
+                                                    </li>
+                                                </ol>
+                                            </td>
+                                            <td>
+                                                Aktif
+                                            </td>
+                                            <td class="space-nowrap">
+                                                <a href="#" class="btn btn-primary btn-sm">Detail</a>
+                                                <button onclick="" class="btn btn-danger btn-sm">Nonaktifkan</button>
+                                                <button onclick="" class="btn btn-warning btn-sm">Aktifkan</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready(function() {
+            $('#studentTable').DataTable({});
+        });
+    </script>
+@endsection
