fb52a9797b764ecb512f1847b91d356c42cbd26b
RafliFerdian25 <ferdianrafli25@gmail.com>
2023-06-08 03:24:11 +0700

    `fix` data course playing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -69,7 +69,9 @@ class DashboardController extends Controller
     public function profile()
     {
         $user = Auth::user()->customer;
-        $profile = Customer::withCount('studentCourseEnrolls', 'carts')->where('id', $user->id)->first();
+        $profile = Customer::withCount(["studentCourseEnrolls as student_course_enrolls_count" => function ($query) {
+            $query->whereIn("status", ["aktif", "selesai"]);
+        }], 'carts')->where('id', $user->id)->first();
         $data = [
             'title' => 'Akun Saya | UMKMPlus',
             'active' => 'account',
