984cb00d7738b87246891f7c3887af47f1309255
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-06-09 04:34:33 +0700

    `fixing` navbar admin

--- resources/views/admin/components/navbar.blade.php
+++ resources/views/admin/components/navbar.blade.php
@@ -189,16 +189,17 @@
         <li class="nav-item dropdown hidden-caret">
             <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
                 <div class="avatar-sm">
-                    <img src="{{ asset('assets/template/admin/img/profile.jpg') }}" alt="profile-admin"
-                        class="avatar-img rounded-circle">
+                    <img src="{{ asset(auth()->user()->customer->profile_picture) }}"
+                        alt="{{ auth()->user()->username }}-profile" class="avatar-img rounded-circle">
                 </div>
             </a>
             <ul class="dropdown-menu dropdown-user animated fadeIn">
                 <div class="dropdown-user-scroll scrollbar-outer">
                     <li>
                         <div class="user-box">
-                            <div class="avatar-lg"><img src="{{ asset('assets/template/admin/img/profile.jpg') }}"
-                                    alt="image profile" class="avatar-img rounded"></div>
+                            <div class="avatar-lg"><img src="{{ asset(auth()->user()->customer->profile_picture) }}"
+                                    alt="{{ auth()->user()->username }}-profile" class="avatar-img rounded">
+                            </div>
                             <div class="u-text">
                                 <h4>{{ Auth::user()->username }}</h4>
                                 <p class="text-muted">{{ Auth::user()->email }}</p><a href="/admin/nameAdmin"
