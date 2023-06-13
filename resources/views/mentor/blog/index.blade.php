4e54410be020b13c4c13da7891d7fb3548ad373d
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-06-13 14:11:45 +0700

    `fixing` blog mentor

--- resources/views/mentor/blog/index.blade.php
+++ resources/views/mentor/blog/index.blade.php
@@ -16,7 +16,7 @@
                         <i class="flaticon-right-arrow"></i>
                     </li>
                     <li class="nav-item">
-                        <a href="/mentor/blog">Data Blog</a>
+                        <a href="#">Data Blog</a>
                     </li>
                 </ul>
             </div>
